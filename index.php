81e5e582e140014efd95773bbd637eddb599656b
g.skrylnikov <grisha2182@gmail.com>
2024-10-09 22:12:06 +0300

    fixes

--- index.php
+++ index.php
@@ -5,17 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="/styles/table/style.css">
     <link rel="stylesheet" href="/styles/main/style.css">
 </head>
 
-<body>
-    <h1 class="title">Расписание ИСИТ</h1>
-    <div class="change-block">
-        <? for ($i = 1; $i < 5; $i++): ?>
-            <div class="change-block__item">
-                <a href="table.php/?course_number=<?= $i ?>"><?= $i ?> курс</a>
-            </div>
-        <? endfor; ?>
+<body style="height: 80vh;">
+    <div class="iframe-block">
+        <iframe src="http://kyrsach/excel.php/?course_number=<?= $_GET["course_number"] ?>" frameborder="0" width="80%" height="100%"
+            style="margin: 0 auto;"></iframe>
     </div>
 </body>
 
